ff16b07a7121fced94290325303b9ce6efd3890c
arunavan <arunajava567@gmail.com>
2024-07-09 20:19:23 +0530

    Add files via upload

---
+++ day1/app1.py
@@ -0,0 +1,19 @@
+print("python codeing..")
+
+a1=20.78
+print(a1);
+print(type(a1))
+print(bin(4))
+
+x=5+3j
+y=4+2j
+print(x+y)
+
+lang='python'
+print(lang)
+print(type(lang))
+
+
+print(456456.345435)
+print(int(456456.345435))
+
